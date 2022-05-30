16129d2c47526fe990dd4acdbb8120d6703d2c5e
Mart7anova <mart7anova7@gmail.com>
2022-05-30 16:25:10 +0300

    5 HW done, new styles

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
 import {PATH} from './Pages';
+import './Header.css'
 
 function Header() {
     return (
         <div>
-            <NavLink to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
-            <NavLink to={PATH.JUNIOR}>Junior</NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS}>Junior-plus</NavLink>
+            <input type="checkbox" id="nav-toggle"/>
+            <label htmlFor="nav-toggle"><span></span></label>
+
+            <ul className="b-menu">
+                <li><NavLink className='link' to={PATH.PRE_JUNIOR}>PreJunior</NavLink></li>
+                <li><NavLink className='link' to={PATH.JUNIOR}>Junior</NavLink></li>
+                <li><NavLink className='link' to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink></li>
+            </ul>
         </div>
     )
 }
 
 export default Header
 
+/*
+<NavLink to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+<NavLink to={PATH.JUNIOR}>Junior</NavLink>
+<NavLink to={PATH.JUNIOR_PLUS}>Junior-plus</NavLink>*/
